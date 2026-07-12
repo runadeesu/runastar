3e98b64d77b63490ec281a1614600ed45ca72e36
runadeesu <sekainoruna@protonmail.com>
2026-07-12 14:02:51 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE = 'runastar-v1';
-const ASSETS = ['./','./index.html','./styles.css','./app.js','./firebase.js','./manifest.json'];
-self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))); self.clients.claim(); });
-self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))); });
